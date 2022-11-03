a703e5a6a6d9762bc880029405d91e48b004abbd
Yslamguly <islamguly28@gmail.com>
2022-11-03 16:51:43 +0100

    feat<shopping_cart>: api for adding and deleting books from shopping_cart

--- src/controllers/shopping_cart.controller.js
+++ src/controllers/shopping_cart.controller.js
@@ -3,7 +3,8 @@ const db = require('../../config/db')
 exports.getUserShoppingCart = (req,res)=>{
     const customer_id = req.user[0].id;
     const shopping_cart = 'bookstore.shopping_cart'
-    db.select('isbn','author','title','publication_year','selling_price','image')
+
+    db.select('id','isbn','author','title','publication_year','selling_price','image')
         .from('bookstore.books')
         .whereIn('bookstore.books.id',function(){
             this.select('book_id').from('bookstore.shopping_cart_item')
@@ -13,4 +14,34 @@ exports.getUserShoppingCart = (req,res)=>{
         })
         .then(data=>res.send(data))
         .catch(err=>res.status(400).send(err))
+}
+
+exports.addBookToShoppingCart = (req,res)=>{
+    const{book_id} = req.body;
+    const customer_id = req.user[0].id;
+    db.raw(`insert into bookstore.shopping_cart_item (cart_id, book_id ) 
+        SELECT shopping_cart.id, books.id 
+        FROM   
+        (SELECT id 
+        FROM   bookstore.shopping_cart 
+        WHERE  customer_id = ${customer_id}) AS shopping_cart, 
+        (SELECT id
+        FROM   bookstore.books
+        WHERE  id = ${book_id}) AS books`)
+        .then(data=>res.status(201).json('Book has been added successfully'))
+        .catch(err=>res.status(400).json('This book is already in your cart'))
+}
+
+exports.deleteBookFromShoppingCart = (req,res)=>{
+    const{shopping_cart_item_book_id} = req.body;
+    const customer_id = req.user[0].id;
+    const shopping_cart_id = db.select('id').from('bookstore.shopping_cart').where('customer_id','=',customer_id)
+    const book_id = db.select('id').from('bookstore.books').where('id','=',shopping_cart_item_book_id)
+
+    db('bookstore.shopping_cart_item')
+        .where('cart_id','=',shopping_cart_id)
+        .andWhere('book_id','=',book_id)
+        .del()
+        .then(response=>res.status(200).json('Book has been deleted successfully'))
+        .catch(err=>res.status(400).json(err))
 }
--- src/routes/shopping_cart.route.js
+++ src/routes/shopping_cart.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const shoppingCartController = require('../controllers/shopping_cart.controller')
 const auth = require('../helpers/auth')
-const {checkNotAuthenticated} = require("../helpers/auth");
+const {checkNotAuthenticated, checkAuthenticated} = require("../helpers/auth");
 
 router.get('/',checkNotAuthenticated,shoppingCartController.getUserShoppingCart);
-
+router.post('/addBook',checkNotAuthenticated,shoppingCartController.addBookToShoppingCart);
+router.delete('/deleteBook',checkNotAuthenticated,shoppingCartController.deleteBookFromShoppingCart);
 
 module.exports = router;
 
