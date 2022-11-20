4ada4f7415f48cf27ffc777b76f0f2a312808d19
Yslamguly <islamguly28@gmail.com>
2022-11-20 11:29:07 +0100

    added route for creating order

---
+++ src/controllers/orders.controller.js
@@ -0,0 +1,26 @@
+const db = require('../../config/db')
+
+exports.createShopOrder = (req,res)=>{
+    const customer_id = req.user[0].id
+
+    const {customer_address_id,final_price} = req.body;
+    db.transaction(trx=>{
+        trx.insert({
+            customer_id:customer_id,
+            order_date:new Date(),
+            final_price:final_price,
+            customer_address_id:customer_address_id
+        }).into('bookstore.shop_order').returning('id')
+            .then(order=>{
+                return trx.raw(`insert into bookstore.order_item (shop_order_id,book_id,quantity,total_price)  
+                                 select ${order[0].id} as shop_order_id,book_id,quantity,total_price 
+                                 from bookstore.shopping_cart_item sci 
+                                 join bookstore.shopping_cart sc  on sc.id  = sci.cart_id 
+                                 where sc.customer_id = ${customer_id}`)
+                    .then(res.status(200).json({message:'Successfully created order'}))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+            .catch(err=>res.send(err))
+    }).catch(err => res.status(500).json({message:'Internal server error'}))
+}
