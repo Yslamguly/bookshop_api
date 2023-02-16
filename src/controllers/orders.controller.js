e6d806fcb956392e068247d21fa1a98d35a511d1
Yslamguly <islamguly28@gmail.com>
2023-02-16 05:59:37 +0100

    function to add a user order to db after payment completed

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -24,3 +24,24 @@ exports.createShopOrder = (req,res)=>{
             .catch(err=>res.send(err))
     }).catch(err => res.status(500).json({message:'Internal server error'}))
 }
+
+exports.createOrder = (customer_id,final_price)=>{
+    db.transaction(trx=>{
+        trx.insert({
+            customer_id:customer_id,
+            order_date:new Date(),
+            final_price:final_price,
+        }).into('bookstore.shop_order').returning('id')
+            .then(order=>{
+                return trx.raw(`insert into bookstore.order_item (shop_order_id,book_id,quantity,total_price)  
+                                 select ${order[0].id} as shop_order_id,book_id,quantity,total_price 
+                                 from bookstore.shopping_cart_item sci 
+                                 join bookstore.shopping_cart sc  on sc.id  = sci.cart_id 
+                                 where sc.customer_id = ${customer_id}`)
+                    .then()
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+            .catch(err=> console.log(err))
+    }).catch(err => console.log(err))
+}
