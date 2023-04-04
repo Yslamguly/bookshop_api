18a1480e55ae2009237002a7c6983ad22d088639
Yslamguly <islamguly28@gmail.com>
2023-04-04 14:53:12 +0200

    deleted unnecessary code

--- src/routes/customers.route.js
+++ src/routes/customers.route.js
@@ -8,21 +8,3 @@ router.post('/login',customersController.login)
 router.put('/:passwordResetCode/reset-password',customersController.resetPassword)
 
 module.exports = router;
-
-
-
-
-
-
-// router.get('/login',auth.checkAuthenticated,customersController.login)
-//
-// router.post('/login',passport.authenticate("local", {
-//         successReturnToOrRedirect: "/home",
-//         failureRedirect: 'login',
-//         failureFlash: true,
-//         successFlash: 'Successful!'
-//     }))
-// router.get('/login/federated/google', customersController.signinGoogle);
-// router.get('/oauth2/redirect/google',customersController.googleCallback);
-// router.get('/',customersController.test);
-// router.get('/home',auth.checkNotAuthenticated,customersController.home)
