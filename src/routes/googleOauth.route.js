b1bd5c97ab8116fad82193f04813f53482773052
Yslamguly <islamguly28@gmail.com>
2023-04-04 13:04:57 +0200

    feat<googleOauth>: added google/callback route

--- src/routes/googleOauth.route.js
+++ src/routes/googleOauth.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const googleOauthController = require('../controllers/googleOauthController')
 
 router.get('/google/url',googleOauthController.getGoogleOauthUrl);
-
+router.get('/google/callback',googleOauthController.googleOauthCallback);
 
 module.exports = router;
