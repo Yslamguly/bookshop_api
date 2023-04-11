10edb0d2378c463cd2a947ac174f784a7a88a953
Yslamguly <islamguly28@gmail.com>
2023-04-11 06:17:05 +0200

    call custom signJwtToken function

--- src/controllers/verify_email.controller.js
+++ src/controllers/verify_email.controller.js
@@ -1,32 +1,17 @@
-const {sendEmail} = require('../helpers/sendEmail')
 const db = require('../../config/db')
 const tableName = require('../../config/table_names.json')
-const jwt = require("jsonwebtoken");
+const {signJwtToken} = require("../helpers/jwtTokenHandler");
 
-exports.testEmail = async (req,res) => {
-    try{
-        await sendEmail({
-            to:'islamguly28+test1@gmail.com',
-            from: 'islamguly28@gmail.com',
-            subject: "Does this work?",
-            text: 'Yes it works'
-        });
-        res.sendStatus(200)
-    }catch (e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-}
 const setIsVerifiedColumnToTrue = async (id) => {
     db(tableName.customers)
-        .where(`${tableName.customers}.id`,'=',id)
-        .update({isVerified:true})
-        .then(()=>console.log('success'))
-        .catch((e)=>console.log(e))
+        .where(`${tableName.customers}.id`, '=', id)
+        .update({isVerified: true})
+        .then(() => console.log('success'))
+        .catch((e) => console.log(e))
 }
 
 
-exports.verifyEmail = async (req,res)=>{
+exports.verifyEmail = async (req, res) => {
     const {verificationString} = req.body
 
     const result = await db.select(`${tableName.customers}.id`,
@@ -37,18 +22,14 @@ exports.verifyEmail = async (req,res)=>{
         `${tableName.customers}.isVerified`,
         `${tableName.customers}.verification_string`)
         .from(`${tableName.customers}`)
-        .where(`${tableName.customers}.verification_string`,'=',verificationString)
+        .where(`${tableName.customers}.verification_string`, '=', verificationString)
 
     await setIsVerifiedColumnToTrue(result[0].id)
 
     const expiry_time = req.session.cookie.originalMaxAge / 100 //6000 seconds
-    jwt.sign(result[0],
-        process.env.JWT_SECRET,
-        {expiresIn : `${expiry_time}s`}, //expires in 10 minutes
-        (err,token) =>
-        {
-            if(err){ return res.status(500).send(err)}
-            res.status(200).json({token})
-        })
+    await signJwtToken(result[0], expiry_time)
+        .then((token) => res.status(200).json({token}))
+        .catch((e) => res.status(500).send(e))
+
     console.log(result[0])
 }
